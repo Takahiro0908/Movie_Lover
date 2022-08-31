226376b3e4a17d74095982d9a8daed0f80563199
Takahiro0908 <fortissimoage@gmail.com>
2022-08-31 06:42:13 +0000

    TMDB_API_KEYをenvに記載

--- app/controllers/public/movies_controller.rb
+++ app/controllers/public/movies_controller.rb
@@ -1,7 +1,11 @@
 class Public::MoviesController < ApplicationController
   before_action :search_movie, only: [:index, :search]
   require 'themoviedb-api'
-  Tmdb::Api.key("d172e3134504081346e78aaedb5d7059")
+  #APIのURL作成
+  api_url = "https://api.themoviedb.org/3/movie/550?api_key=#{ENV['TMDB_API_KEY']}"
+  #TMDB APIにリクエスト
+  uri = URI.parse(api_url)
+
   Tmdb::Api.language("ja") # こちらで映画情報の表示の際の言語設定を日本語にできます
 
   def new
